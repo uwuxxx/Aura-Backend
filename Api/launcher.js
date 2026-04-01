834b4fa045c3e1e177e089af635b7456ab9c3e7a
sh4rk <purnabhandari95@yahoo.com>
2026-04-01 11:08:02 -0400

    Update launcher.js

--- Api/launcher.js
+++ Api/launcher.js
@@ -5,7 +5,7 @@ const log = require("../structs/log.js");
 const bcrypt = require("bcrypt");
 
 //Api for launcher login (If u want a POST requesto just replace "app.get" to "app.post" and "req.query" to "req.body")
-app.get("/api/launcher/login", async (req, res) => {
+app.get("/api/login", async (req, res) => { // will be localhost:3551/api/login
     const { email, password } = req.query;
 
     if (!email) return res.status(400).send('The email was not entered.');
